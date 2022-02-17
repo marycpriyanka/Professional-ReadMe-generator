62f6551766091b3f6a82274dc4d9babfdd8e5670
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-02-17 10:29:26 -0800

    Generate README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Professional-ReadMe-generator
+# Professional README generator
+![badge](https://img.shields.io/badge/MIT-License-blue.svg)
+
+## Description
+
+This is a command line application to generate a professional and high quality README file for a project, quickly. This allows the project creator to devote more time to working on the project. The developer will be prompted with some questions for information about their application repository. Based on the user input, a README.md is generated with the title of the project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions. The badge for the application license is also added below the title  of the README file.
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+To install the dependencies (Inquirer), run 'npm install' in your command line. Then, to invoke the application, use the command 'node index.js'.
+
+## Usage
+
+When the application is invoked, user will be prompted with some questions about the project. Answer the questions correctly. After all questions are answered, the README file will be generated based on user input. If README.md is generated successfully, a message 'Successfully created ReadMe file!' will be logged to the terminal. Look for the README file created in the folder.
+
+## License
+
+Professional README generator is available under the MIT License.
+
+## How to Contribute
+
+Contributions and ideas are welcome. Before submitting an issue, please take a moment to look over the contributing guidelines in https://www.contributor-covenant.org/ . Before submitting pull requests, ensure the following:
+
+## Tests
+
+
+
+## Questions
+
+https://github.com/marycpriyanka
+
+For any addditional questions, reach me at marycpriyanka@gmail.com.
--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const createReadMeText = (title, description, installation, usage, license, contribution, test, username, email) => {
+    const badge = `https://img.shields.io/badge/${license}-blue.svg`;
+    license = license.replace("-", " ");
+    license = license.replace("_", " ");
+    return `# ${title}
+![badge](${badge})
+
+## Description
+
+${description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## License
+
+${title} is available under the ${license}.
+
+## How to Contribute
+
+${contribution}
+
+## Tests
+
+${test}
+
+## Questions
+
+https://github.com/${username}
+
+For any addditional questions, reach me at ${email}.`
+} 
+
 // Takes the user input using inquirer
 inquirer.prompt([
     {
@@ -37,9 +84,9 @@ inquirer.prompt([
         message: "Choose license for your application.",
         type: "list",
         name: "license",
-        choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", 'BSD 2-Clause "Simplified" License', 
-            'BSD 3-Clause "New" or "Revised" License', "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0",
-            "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
+        choices: ["None", "Apache-License_2.0", "GNU-General-Public-License_v3.0", "MIT-License", 'BSD-2_Clause-"Simplified"-License', 
+            'BSD-3_Clause-"New"-or-"Revised"-License', "Boost-Software-License_1.0", "Creative-Commons-Zero_v1.0-Universal", "Eclipse-Public-License_2.0",
+            "GNU-Affero-General-Public-License_v3.0", "GNU-General-Public-License_v2.0", "GNU-Lesser-General-Public-License_v2.1", "Mozilla-Public-License_2.0", "The Unlicense"]
     },
     {
         message: "Enter your GitHub username.",
@@ -52,7 +99,17 @@ inquirer.prompt([
         name: "email"
     }
 ]).then (function (response) {
-
+    const readMeText = createReadMeText(response.title, response.description, response.installation, response.usage, response.license, 
+        response.contribution, response.test, response.username, response.email);
+    fs.writeFile("README.md", readMeText, err => {
+        if (err) {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Successfully created ReadMe file!");
+        }
+    })
 }).catch (function (error) {
     console.log(error);
 });
